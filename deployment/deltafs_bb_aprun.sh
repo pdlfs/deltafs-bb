7380f80524d0f7319432207f94463e995c93831c
Saurabh Kadekodi <saukad@h1.fs.tablefs.narwhal.pdl.cmu.edu>
2017-02-06 09:36:07 -0500

    Fixed aprun script

--- deployment/deltafs_bb_aprun.sh
+++ deployment/deltafs_bb_aprun.sh
@@ -16,8 +16,6 @@
 
 num_bbos_client_nodes=8
 num_bbos_server_nodes=2
-# num_bbos_client_nodes=8
-# num_bbos_server_nodes=2
 
 # Paths
 umbrella_build_dir="$HOME/src/deltafs-umbrella/build"
@@ -25,24 +23,19 @@ deltafs_dir="$HOME/src/deltafs-bb"
 output_dir="$HOME/src/deltafs-bb/dump" # SHOULD be burst buffer path
 config_dir="$output_dir/config"
 mkdir $config_dir
-container_dir="$output_dir/containers"
-mkdir $container_dir
-rm -rf $container_dir/*
 logfile="" # don't put in container_dir because we delete it
 # umbrella_build_dir="/users/saukad/devel/deltafs-bb/build"
 # deltafs_dir="/users/saukad/devel/deltafs-bb"
 # output_dir="/tmp/bb"
 # config_dir="$output_dir/config"
 # mkdir $config_dir
-# container_dir="$output_dir/containers"
-# mkdir $container_dir
-# rm -rf $container_dir/*
 # logfile=""
 
 ###############
 # Core script #
 ###############
 
+# Client transfer sizes we experiment with
 OBJECT_CHUNK_SIZE[0]=1048576 # 1 MB
 OBJECT_SIZE[0]=62914560 # 60 MB
 
@@ -55,11 +48,12 @@ OBJECT_SIZE[2]=62914560 # 60 MB
 OBJECT_CHUNK_SIZE[3]=15728640 # 15 MB
 OBJECT_SIZE[3]=62914560 # 60 MB
 
-PFS_CHUNK_SIZE[0] = 1048576
-PFS_CHUNK_SIZE[1] = 2097152
-PFS_CHUNK_SIZE[2] = 4194304
-PFS_CHUNK_SIZE[3] = 8388608
-PFS_CHUNK_SIZE[4] = 16777216
+# PFS sizes we experiment with
+PFS_CHUNK_SIZE[0]=1048576
+PFS_CHUNK_SIZE[1]=2097152
+PFS_CHUNK_SIZE[2]=4194304
+PFS_CHUNK_SIZE[3]=8388608
+PFS_CHUNK_SIZE[4]=16777216
 
 # OBJECT_CHUNK_SIZE=2
 # OBJECT_SIZE=2048
@@ -68,40 +62,41 @@ message () { echo "$@" | tee $logfile; }
 die () { message "Error $@"; exit 1; }
 
 # Generate aprun -L comma-separated node lists
-bbos_client_nodes = $(cat $PBS_NODEFILE | uniq | sort | head -n $num_bbos_client_nodes | tr '\n' ',')
-bbos_server_nodes = $(cat $PBS_NODEFILE | uniq | sort | tail -n $num_bbos_server_nodes | tr '\n' ',')
-# bbos_client_nodes="localhost"
-# bbos_server_nodes="localhost"
-
-# Get client and server IPs
-message "Getting client IP addresses..."
-aprun -L $bbos_client_nodes -n 1 -N 1 hostname -i > $output_dir/bbos_client_ip.txt
-# mpirun --host $bbos_client_nodes hostname -i > $output_dir/bbos_client_ip.txt
-
-message "Getting server IP addresses..."
-aprun -L $bbos_server_nodes -n 1 -N 1 hostname -i > $output_dir/bbos_server_ip.txt
-# mpirun --host $bbos_server_nodes hostname -i > $output_dir/bbos_server_ip.txt
+# PBS_NODEFILE="fakenodes.txt"
+bbos_client_nodes=$(cat $PBS_NODEFILE | uniq | sort | head -n $num_bbos_client_nodes | tr '\n' ',')
+bbos_server_nodes=$(cat $PBS_NODEFILE | uniq | sort | tail -n $num_bbos_server_nodes | tr '\n' ',')
+echo $bbos_client_nodes
+echo $bbos_server_nodes
 
 bbos_client_path="$umbrella_build_dir/bin/bbos_client"
 bbos_server_path="$umbrella_build_dir/bin/bbos_server"
-# bbos_client_config_name="trinitite_client.conf"
-# bbos_server_config_name="trinitite_server.conf"
-bbos_client_config_name="narwhal_client.conf"
-bbos_server_config_name="narwhal_server.conf"
+# bbos_client_path="$umbrella_build_dir/src/bbos_client"
+# bbos_server_path="$umbrella_build_dir/src/bbos_server"
+bbos_client_config_name="trinitite_client.conf"
+bbos_server_config_name="trinitite_server.conf"
+# bbos_client_config_name="narwhal_client.conf"
+# bbos_server_config_name="narwhal_server.conf"
 bbos_client_config="$deltafs_dir/config/$bbos_client_config_name"
 bbos_server_config="$deltafs_dir/config/$bbos_server_config_name"
 
 # Now start servers and clients
-server_num=0
-declare -a server_pids
-num_skip=0
 num_bbos_clients_per_server=$(($num_bbos_client_nodes / $num_bbos_server_nodes))
 p=0
 for pfs_size in ${PFS_CHUNK_SIZE[@]}
 do
+  message ""
+  message "========= TESTING FOR PFS CHUNK SIZE ${PFS_CHUNK_SIZE[$p]} ========="
   s=0
   for sizes in ${OBJECT_CHUNK_SIZE[@]}
   do
+    message ""
+    message "========= TESTING FOR OBJ CHUNK SIZE ${OBJECT_CHUNK_SIZE[$s]} ========="
+    server_num=0
+    declare -a server_pids
+    num_skip=0
+    n=0
+    container_dir=$output_dir/containers-${OBJECT_CHUNK_SIZE[$s]}-${PFS_CHUNK_SIZE[$p]}
+    mkdir $container_dir
     for bbos_server in $(echo $bbos_server_nodes | sed "s/,/ /g")
     do
       # copying config files for every server
@@ -113,7 +108,6 @@ do
       # mpirun --host $bbos_server $bbos_server_path $config_dir/$bbos_server_config_name.$bbos_server & 2>&1 | tee $logfile
       server_pids[$server_num]=$(echo $!)
       sleep 1
-
       message "Started BBOS server $bbos_server with PID ${server_pids[$server_num]}."
       j=0
       for bbos_client in $(echo $bbos_client_nodes | sed "s/,/ /g")
@@ -145,12 +139,12 @@ do
     for bbos_server in $(echo $bbos_server_nodes | sed "s/,/ /g")
     do
       pid=${server_pids[$i]}
+      # killing servers to perform final binpackings
       message "Killing BBOS server on host $bbos_server"
       aprun -L $bbos_server -n 1 -N 1 -d 1 pkill -SIGINT bbos_server 2>&1 | tee $logfile
       # mpirun --host $bbos_server pkill -SIGINT bbos_server 2>&1 | tee $logfile
       i=$(($i + 1))
     done
-    rm -rf $container_dir/*
     s=$(($s + 1))
   done
   p=$(($p + 1))
