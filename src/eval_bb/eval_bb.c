e7923f0be26d2074084c24e890e1cb6a06c6a099
Chuck Cranor <chuck@ece.cmu.edu>
2017-03-08 23:25:25 -0500

    resolve warnings

--- src/eval_bb/eval_bb.c
+++ src/eval_bb/eval_bb.c
@@ -8,11 +8,15 @@ int main() {
   const char *v = getenv("BB_Lustre_chunk_size");
   unsigned long pchunk = strtoul(v, NULL, 0);
   char command[512];
+  unsigned long container_size;
+  uint64_t count;
+  int ret;
+
   v = getenv("BB_Max_container_size");
-  unsigned long container_size = strtoul(v, NULL, 0);
-  uint64_t count = (container_size / pchunk);
+  container_size = strtoul(v, NULL, 0);
+  count = (container_size / pchunk);
   snprintf(command, 512, "dd if=/dev/zero of=%s bs=%lu count=%lu", fname, pchunk, count);
-  int ret = system(command);
+  ret = system(command);
   assert(ret == 0);
-  return 0;
+  return(ret);
 }
