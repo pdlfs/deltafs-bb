74c872ad92fe5c77684f369bdcbcfda53ea9fc16
Saurabh Kadekodi <saukad@cs.cmu.edu>
2017-03-14 13:38:38 -0400

    Added client stats for per-append latency

--- src/bbos_client/bbos_client.cc
+++ src/bbos_client/bbos_client.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -21,7 +22,35 @@ static const char alphanum[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 "abcdefghijklmnopqrstuvwxyz";
 
+#ifndef CLOCK_REALTIME
+#define CLOCK_REALTIME 0
+static int clock_gettime(int id, struct timespec *tp) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  tp->tv_sec = tv.tv_sec;
+  tp->tv_nsec = tv.tv_usec * 1000;
+  return (0);
+}
+#endif
+
+static void timespec_diff(struct timespec *start, struct timespec *stop,
+                          struct timespec *result) {
+  if ((stop->tv_nsec - start->tv_nsec) < 0) {
+    result->tv_sec = stop->tv_sec - start->tv_sec - 1;
+    result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
+  } else {
+    result->tv_sec = stop->tv_sec - start->tv_sec;
+    result->tv_nsec = stop->tv_nsec - start->tv_nsec;
+  }
+  return;
+}
+
 int stringLength = sizeof(alphanum) - 1;
+
+static double avg_hg_chunk_response_time = 0.0;
+static uint64_t num_hg_chunks_written = 0;
+static timespec hg_chunk_ts_before;
+static timespec hg_chunk_ts_after;
 
 char genRandom() {
   return alphanum[rand() % stringLength];
@@ -31,9 +60,19 @@ void write_data(bbos_handle_t bc, const char *name, char *input,
                 int num_chars) {
   size_t total_data_written = 0;
   size_t data_written = 0;
+  timespec hg_chunk_diff_ts;
   do {
+    clock_gettime(CLOCK_REALTIME, &hg_chunk_ts_before);
     data_written = bbos_append(bc, name, (void *)(input + total_data_written),
                                num_chars - total_data_written);
+
+    clock_gettime(CLOCK_REALTIME, &hg_chunk_ts_after);
+    timespec_diff(&hg_chunk_ts_before, &hg_chunk_ts_after, &hg_chunk_diff_ts);
+    num_hg_chunks_written += 1;
+    avg_hg_chunk_response_time *= (num_hg_chunks_written - 1);
+    avg_hg_chunk_response_time +=
+        ((hg_chunk_diff_ts.tv_sec * 1000000000) + hg_chunk_diff_ts.tv_nsec);
+    avg_hg_chunk_response_time /= num_hg_chunks_written;
     total_data_written += data_written;
   } while(total_data_written < num_chars);
 }
@@ -47,6 +86,12 @@ void read_data(bbos_handle_t bc, const char *name, char *output,
                           total_data_read, num_chars - total_data_read);
     total_data_read += data_read;
   } while(total_data_read < num_chars);
+}
+
+void print_stats(const char *obj_name, size_t chunk_size) {
+  printf("######## CLIENT STATS: HG_CHUNK_SIZE: %lu, OBJECT NAME: %s ##########\n", chunk_size, obj_name);
+  printf("HG_CHUNK APPEND AVG. RESPONSE TIME = %f ns\n", avg_hg_chunk_response_time);
+  printf("##############################################\n");
 }
 
 int main(int argc, char **argv) {
@@ -132,5 +177,7 @@ int main(int argc, char **argv) {
   // read_data(bc, argv[1], output, chunk_size * num_chunks);
   // printf("data read = %s\n", output);
   bbos_finalize(bc);
+
+  print_stats((const char *)obj_name, chunk_size);
   return 0;
 }
